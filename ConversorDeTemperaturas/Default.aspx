7256e0471e0403be0a7ee099daba4b24e5ae0fb1
AdrianZevallos <70779893@ucsm.edu.pe>
2021-09-01 09:21:21 -0500

    Correccion de experiencia nro 1 y 2

---
+++ ConversorDeTemperaturas/Default.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="ConversorDeTemperaturas._Default" %>
+
---
+++ ConversorDeTemperaturas/Site.Master.vb
@@ -0,0 +1,22 @@
+﻿Public Class SiteMaster
+    Inherits MasterPage
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = (Val(TextBox1.Text) - 32) * (5 / 9)
+        TextBox3.Text = (Val(TextBox1.Text) - 32) * (1.8)
+    End Sub
+
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+End Class
+
